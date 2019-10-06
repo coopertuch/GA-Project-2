2cab3e9935dbf1a32286cdf5dbd40a6f9643692c
Cooper Tuch <coopertuch@gmail.com>
2019-10-06 15:04:31 -0700

    fixed hbs issues, updating css

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const hbs = require('hbs')
+const express = require("express")
+const hbs = require("hbs")
 const app = express()
-const parser = require('body-parser')
-const methodOverride = require('method-override')
+const parser = require("body-parser")
+const methodOverride = require("method-override")
 
-app.use(methodOverride('_method'))
-app.use('/assets', express.static('public'))
+app.use(methodOverride("_method"))
+app.use("/assets", express.static("public"))
 app.use(parser.urlencoded({ extended: true }))
 app.set("view engine", "hbs")
 
-const myController = require('./controllers/myController')
+const myController = require("./controllers/myController")
 app.use("/", myController)
 
 app.listen(4000, () => console.log("Major Tom to Ground Control"))
