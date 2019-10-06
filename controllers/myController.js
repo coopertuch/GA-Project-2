51653f785c43b2376b9c35826a5afd3b8785a22a
Cooper Tuch <coopertuch@gmail.com>
2019-10-06 14:43:26 -0700

    css edits

--- controllers/myController.js
+++ controllers/myController.js
@@ -25,14 +25,10 @@ router.post('/new', (req, res) => {
       })
 })
 
-// router.get('/edit', (req, res) => {
-//   res.render('edit')
-// })
-
 router.get('/edit/:id', (req, res) => {
     cheerupsModel.findOne({_id: req.params.id})
       .then(cheerup => {
-        res.render("edit", { cheerup })
+        res.render('edit', { cheerup })
       })
 })
 
