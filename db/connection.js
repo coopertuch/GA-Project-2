7a56199d8966dbaf66fcba8a8ead3e4f1288ecac
Cooper Tuch <coopertuch@gmail.com>
2019-10-07 20:11:16 -0700

    heroku

--- db/connection.js
+++ db/connection.js
@@ -11,9 +11,6 @@ if (process.env.NODE_ENV === "production") {
 
 mongoose
     .connect(mongoURI, { useNewUrlParser: true })
-    .then(instance =>
-        console.log(`connected to ${instance.connections[0].name}`)
-    )
     .catch(error => console.log('failed', error));
 
 module.exports = mongoose
--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,5 @@
+const mongoose = require("./connection");
+
 const cheerupsModel = require('../models/cheerups')
 const seedData = require("./seeds.json")
 
