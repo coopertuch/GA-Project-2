c8dca70b2b4226f7eb3befbd97f061b0192c7c6f
Cooper Tuch <coopertuch@gmail.com>
2019-10-07 19:59:33 -0700

    heroku

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,19 @@
 const mongoose = require('mongoose');
 mongoose.Promise = Promise
-let mongoURI = "";
 mongoose.set('useUnifiedTopology', true);
 
+let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
   } else {
     mongoURI = "mongodb://localhost/cheerupsdb";
 };
 
-mongoose.connect(mongoURI, { useNewUrlParser: true })
+mongoose
+    .connect(mongoURI, { useNewUrlParser: true })
+    .then(cheerup =>
+        console.log(`connected`)
+    )
     .catch(error => error);
 
 module.exports = mongoose
