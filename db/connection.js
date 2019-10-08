3c1c9986067895b4cc8f7c0851f0b8c3ecdddef8
Cooper Tuch <coopertuch@gmail.com>
2019-10-07 19:33:28 -0700

    heroku

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
   } else {
     mongoURI = "mongodb://localhost/cheerupsdb";
-  }
+};
 
 mongoose.Promise = Promise
 module.exports = mongoose
