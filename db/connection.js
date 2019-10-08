b02023145368405b72bc52c680f14d5e62ae96d6
Cooper Tuch <coopertuch@gmail.com>
2019-10-07 17:25:06 -0700

    heroku

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
-
-mongoose.Promise = Promise
+mongoose.connect('mongodb://localhost/cheerupsdb')
 
 let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
     mongoURI = process.env.DB_URL;
   } else {
     mongoURI = "mongodb://localhost/cheerupsdb";
-}
+  }
 
-mongoose.connect('mongodb://localhost/cheerupsdb')
-
-
+mongoose.Promise = Promise
 module.exports = mongoose
