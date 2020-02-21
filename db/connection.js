b073dce96ca9ef7572d52e92974d5936bcc40efa
Cooper Tuch <coopertuch@gmail.com>
2020-02-20 20:46:40 -0800

    heroku

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,19 @@
 const mongoose = require("mongoose");
-mongoose.Promise = Promise
+mongoose.Promise = Promise;
 
 let mongoURI = "";
 mongoose.set("useUnifiedTopology", true);
 
 if (process.env.NODE_ENV === "production") {
-    mongoURI = process.env.DB_URL;
-  } else {
-    mongoURI = "mongodb://localhost/cheerupsdb";
-};
+  mongoURI = process.env.DB_URL;
+} else {
+  mongoURI =
+    "mongodb+srv://coopertuch:aufflvC92WYg7MC5@cluster0-gr3pm.mongodb.net/test?retryWrites=true&w=majority";
+}
 
 mongoose
-    .connect(mongoURI, { useNewUrlParser: true })
-    .then(instance => console.log(`connected to db ${instance}`))
-    .catch(error => console.log("failed", error));
+  .connect(mongoURI, { useNewUrlParser: true })
+  .then(instance => console.log(`connected to db ${instance}`))
+  .catch(error => console.log("failed", error));
 
-module.exports = mongoose
+module.exports = mongoose;
