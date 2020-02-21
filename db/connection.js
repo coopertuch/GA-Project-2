c341a330ed8481f1b818d2943117e6845d5d7bd7
Cooper Tuch <coopertuch@gmail.com>
2020-02-20 21:30:40 -0800

    heroku

--- db/connection.js
+++ db/connection.js
@@ -8,7 +8,8 @@ mongoose.set("useUnifiedTopology", true);
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
 } else {
-  mongoURI = "mongodb://localhost/cheerupsdb";
+  mongoURI =
+    "mongodb+srv://coop:0c5G2qvr36gkFZjg@cluster0-gr3pm.mongodb.net/test?retryWrites=true&w=majority";
 }
 
 mongoose
@@ -17,3 +18,5 @@ mongoose
   .catch(error => console.log("failed", error));
 
 module.exports = mongoose;
+
+//   "mongodb+srv://coop:0c5G2qvr36gkFZjg@cluster0-gr3pm.mongodb.net/test?retryWrites=true&w=majority";
